97818f825c159d1bac9599652e690babeeecd45b
Irina Shmoylova <shmoilova.irina2015@yandex.ru>
2020-03-24 21:33:53 +0300

    Read file

--- ProjectParser/ProjectParser/Program.cs
+++ ProjectParser/ProjectParser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,19 @@ namespace ProjectParser
 {
     class Program
     {
+        static void Read()
+        {
+            string file_name = "D:\\C#\\Parser\\log.log";
+
+            using (StreamReader sr = new StreamReader(file_name))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+
         static void Main(string[] args)
         {
+            Read();
         }
     }
 }
