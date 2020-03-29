67864922141e57ccf59dffa2f08db99e73eb9854
Irina Shmoylova <shmoilova.irina2015@yandex.ru>
2020-03-29 19:29:41 +0300

    Regex

--- ProjectParser/ProjectParser/Program.cs
+++ ProjectParser/ProjectParser/Program.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ProjectParser
 {
     class Program
     {
-        static void Find_files(string folder_path, string find_word)
+        static void Find_files(string folder_path, string pattern)
         {
             DirectoryInfo d = new DirectoryInfo(folder_path);
             foreach (FileInfo f in d.GetFiles("*.log"))
             {
                 Console.WriteLine(f.Name);
-                Read(folder_path + "\\" + f.Name, find_word);
+                Read(folder_path + "\\" + f.Name, pattern);
             }
         }
-
-        static void Read(string file_name, string find_word)
+        
+        static void Read(string file_name, string pattern)
         {
             using (StreamReader sr = new StreamReader(file_name))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (line.Contains(find_word))
+                    if (Regex.IsMatch(line, pattern, RegexOptions.IgnoreCase))
                     {
                         Write("D://C#//Parser//result.txt", line);
                         Console.WriteLine(line);
